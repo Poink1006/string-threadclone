52462b515d43c58b919c09ebe4746cde757b531d
Daniel Victor Ang <daniel1006ang@gmail.com>
2023-09-02 14:01:51 +0800

    add home features

---
+++ constants/index.js
@@ -0,0 +1,32 @@
+export const sidebarLinks = [
+  {
+    imgUrl: "/assets/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgUrl: "/assets/search.svg",
+    route: "/search",
+    label: "Search",
+  },
+  {
+    imgUrl: "/assets/heart.svg",
+    route: "/activity",
+    label: "Activity",
+  },
+  {
+    imgUrl: "/assets/create.svg",
+    route: "/create-string",
+    label: "Create String",
+  },
+  {
+    imgUrl: "/assets/community.svg",
+    route: "/communities",
+    label: "Communities",
+  },
+  {
+    imgUrl: "/assets/user.svg",
+    route: "/profile",
+    label: "Profile",
+  },
+];
