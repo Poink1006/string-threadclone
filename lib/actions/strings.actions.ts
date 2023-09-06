e28757da52f23d9e9006d0b22650f6b871e418f0
Daniel Victor C. Ang <daniel1006ang@gmail.com>
2023-09-06 23:23:51 +0800

    update

---
+++ lib/actions/strings.actions.ts
@@ -0,0 +1,13 @@
+interface Params {
+  title: string;
+  author: string;
+  community: string;
+  path: string;
+}
+
+export default async function createStrings({
+  title,
+  author,
+  community,
+  path,
+}: Params) {}
